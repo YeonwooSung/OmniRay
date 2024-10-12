64ca036c4fd8f9b3a31ac71c2732995fc9277992
YeonwooSung <neos960518@gmail.com>
2024-10-12 21:45:06 +0900

    feat: Implement the vllm servable info, and design the batch predictor

---
+++ src/omniray/batch_predictor/servable_info/vllm_servable_info.py
@@ -0,0 +1,50 @@
+from vllm import EngineArgs, SamplingParams, AsyncEngineArgs
+from typing import Union
+
+from .base import ServableInfo, Framework, DeviceType
+
+
+class VLLMConfigs:
+    engine_args: Union[EngineArgs, AsyncEngineArgs]
+    sampling_params: SamplingParams
+
+
+class VLLMServableInfo(ServableInfo):
+    def __init__(
+        self,
+        full_path: str,
+        model_name: str,
+        device_type: DeviceType = DeviceType.CUDA,
+        config: Union[VLLMConfigs, None] = None,
+        **kwargs
+    ):
+        super().__init__(
+            full_path=full_path,
+            model_name=model_name,
+            framework=Framework.VLLM,
+            device_type=device_type,
+            **kwargs
+        )
+
+        # check if VLLMConfigs is provided
+        assert config is not None, "VLLMConfigs must be provided."
+
+        if device_type is not DeviceType.CUDA:
+            if device_type is DeviceType.CPU:
+                print("VLLM model is loaded on CPU device.")
+                print("This may cause a significant slowdown in model inference.")
+            else:
+                raise ValueError("VLLM model must be loaded on CUDA device.")
+
+        self.config = config
+
+        if not isinstance(self.config, VLLMConfigs):
+            raise ValueError("config must be an instance of VLLMConfigs.")
+
+        # check if engine_args is either EngineArgs or AsyncEngineArgs, and set is_async_engine
+        if isinstance(self.config.engine_args, EngineArgs):
+            self.is_async_engine = False
+        elif isinstance(self.config.engine_args, AsyncEngineArgs):
+            self.is_async_engine = True
+        else:
+            raise ValueError("engine_args must be an instance of EngineArgs or AsyncEngineArgs.")
---
+++ src/omniray/batch_predictor/vllm_batch_predictor.py
@@ -0,0 +1,25 @@
+from vllm import AsyncLLMEngine, LLMEngine, RequestOutput, SamplingParams
+import ray
+
+from .base import BatchPredictor
+from .servable_info.vllm_servable_info import VLLMServableInfo, VLLMConfigs
+
+
+@ray.remote
+class VLLMBatchPredictor(BatchPredictor):
+    def __init__(self, servable_info: VLLMServableInfo):
+        super().__init__(servable_info)
+
+        config: VLLMConfigs = servable_info.config
+
+        if servable_info.is_async_engine:
+            self.engine = AsyncLLMEngine.from_engine_args(config.engine_args)
+        else:
+            self.engine = LLMEngine(config.engine_args)
+
+    def is_async_engine(self):
+        return self.servable_info.is_async_engine
+
+    def predict(self, data):
+        #TODO Implement VLLM model inference
+        pass
